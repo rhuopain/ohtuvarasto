61a9e6339715ac6a0686b7a2c80dc86189fa5910
Tiia Ignatius <tignati2@local>
2023-10-30 21:18:10 +0200

    Add varasto class tests to conform 100% branch coverage and update .gitignore

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -18,10 +18,20 @@ class TestVarasto(unittest.TestCase):
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
 
-    def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
+    def test_lisays_lisaa_yli_tilavuuden(self):
+        self.varasto.lisaa_varastoon(8)
         self.varasto.lisaa_varastoon(8)
 
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
+        self.varasto.lisaa_varastoon(8)
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
+
+    def test_lisaaminen_negatiivinen_ei_muuta_tilaa(self):
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.lisaa_varastoon(-2)
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
@@ -31,6 +41,12 @@ class TestVarasto(unittest.TestCase):
 
         self.assertAlmostEqual(saatu_maara, 2)
 
+    def test_ottaminen_liikaa_ei_onnistu(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu_maara = self.varasto.ota_varastosta(10)
+
+        self.assertAlmostEqual(saatu_maara, 8)
+
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +54,24 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ottaminen_negatiivinen_ei_onnistu(self):
+        saatu_maara = self.varasto.ota_varastosta(-10)
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    
+    def test_kostruktori_luo_negatiivisen_tilavuuden_saldo_oikein(self):
+        self.varasto = Varasto(-5,-5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_kostruktori_luo_negatiivisen_tilavuuden_tilavuus_oikein(self):
+        self.varasto = Varasto(-5)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+        
+    def test_luokka_tulostuu_oikein(self):
+        self.varasto.lisaa_varastoon(4)
+        self.assertMultiLineEqual(str(self.varasto), "saldo = 4, vielä tilaa 6") 
+        
+        
+        
+    
